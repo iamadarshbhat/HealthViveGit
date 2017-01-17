e2e16c1ded085b18b28731bee191cbca123199f1
iamadarshbhat <adarshas@nousinfo.com>
2017-01-17 15:25:04 +0530

    Initial Commit

---
+++ HealthVive/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  HealthVive
+//
+//  Created by Adarsha on 17/01/17.
+//  Copyright © 2017 NousInfosystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ HealthVive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthVive
+//
+//  Created by Adarsha on 17/01/17.
+//  Copyright © 2017 NousInfosystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
